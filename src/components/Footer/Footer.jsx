ee0c4e70ad558ce4bb23543f25810e7d364febe4
MIGUEL <msabl005@fiu.edu>
2023-05-28 18:16:52 -0400

    Created Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,12 +1,43 @@
 import "./Footer.css";
+import { FaLinkedinIn } from "react-icons/fa";
+import { AiFillGithub } from "react-icons/ai";
+import { IoMdMail } from "react-icons/io";
+import { Link } from "react-scroll";
 
 const Footer = () => {
   return (
     <footer className="footer-section">
-      <h1>Miguel Sablan</h1>
-      <p>&copy; Copyright 2023</p>
+      <Link class="logo" to="home" smooth duration={500}>
+        MIGUEL
+      </Link>
 
-      <div className="projects-container"></div>
+      <div className="footer-icons-container">
+        <a
+          href="https://www.linkedin.com/in/miguel-sablan"
+          target="_blank"
+          className="icon-container"
+        >
+          <FaLinkedinIn size="30px" />
+        </a>
+
+        <a
+          href="https://www.github.com/miguelSablan"
+          target="_blank"
+          className="icon-container"
+        >
+          <AiFillGithub size="30px" />
+        </a>
+
+        <a
+          href="mailto:msabl005@fiu.edu"
+          target="_blank"
+          className="icon-container"
+        >
+          <IoMdMail size="30px" />
+        </a>
+      </div>
+
+      <p>Miguel Sablan &copy; 2023</p>
     </footer>
   );
 };

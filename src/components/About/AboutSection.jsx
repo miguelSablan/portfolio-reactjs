8f16d9ab9a5a90ab22c8d839a227837f2b45d320
MIGUEL <msabl005@fiu.edu>
2023-09-19 15:33:08 -0400

    added skills section to about me

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -2,7 +2,7 @@ import "./AboutSection.css";
 import { FaGitAlt, FaReact } from "react-icons/fa";
 import { AiFillHtml5, AiOutlineDownload } from "react-icons/ai";
 import { DiCss3, DiMongodb } from "react-icons/di";
-import { SiJavascript, SiExpress } from "react-icons/si";
+import { SiJavascript, SiExpress, SiSwift } from "react-icons/si";
 import { IoLogoNodejs } from "react-icons/io";
 import headshotImg from "../../assets/headshot.jpeg";
 import resumePDF from "../../assets/Miguel_Sablan_Resume.pdf";
@@ -34,38 +34,46 @@ const AboutSection = () => {
         <img className="headshot" src={headshotImg} alt="headshot photo" />
       </div>
 
-      {/*
-      <h2 data-aos="fade-up" data-aos-offset="100">
-        Skills
-      </h2>
       <div className="skill-summary" data-aos="fade-up" data-aos-offset="100">
-        <div className="skill-icon">
-          <AiFillHtml5 size={120} color="#e44c26" />
+        <div className="skill">
+          <p className="skill-title">HTML</p>
+          <AiFillHtml5 size={120} color="#e44c26" className="skill-img" />
         </div>
-        <div className="skill-icon">
-          <DiCss3 size={120} color="#1573b6" />
+        <div className="skill">
+          <p className="skill-title">CSS</p>
+          <DiCss3 size={120} color="#1573b6" className="skill-img" />
         </div>
-        <div className="skill-icon">
-          <SiJavascript size={120} color="#f0db4f" />
+        <div className="skill">
+          <p className="skill-title">JavaScript</p>
+          <SiJavascript size={120} color="#f0db4f" className="skill-img" />
         </div>
-        <div className="skill-icon">
-          <FaGitAlt size={120} color="#F1502F" />
+        <div className="skill">
+          <p className="skill-title">Git</p>
+          <FaGitAlt size={120} color="#F1502F" className="skill-img" />
         </div>
 
-        <div className="skill-icon">
-          <DiMongodb size={120} color="#4db33d" />
+        <div className="skill">
+          <p className="skill-title">MongoDB</p>
+          <DiMongodb size={120} color="#4db33d" className="skill-img" />
         </div>
-        <div className="skill-icon">
-          <SiExpress size={120} color="white" />
+        <div className="skill">
+          <p className="skill-title">Express</p>
+          <SiExpress size={120} color="white" className="skill-img" />
         </div>
-        <div className="skill-icon">
-          <FaReact size={120} color="#61dafb" />
+        <div className="skill">
+          <p className="skill-title">React</p>
+          <FaReact size={120} color="#61dafb" className="skill-img" />
         </div>
-        <div className="skill-icon">
-          <IoLogoNodejs size={120} color="#83cd29" />
+        <div className="skill">
+          <p className="skill-title">Node</p>
+          <IoLogoNodejs size={120} color="#83cd29" className="skill-img" />
+        </div>
+
+        <div className="skill">
+          <p className="skill-title">Swift</p>
+          <SiSwift size={120} color="#F1502F" className="skill-img" />
         </div>
       </div>
-  */}
     </section>
   );
 };

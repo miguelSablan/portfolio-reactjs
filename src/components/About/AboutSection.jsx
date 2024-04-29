77ab8cb3eff2ca6d4c81c9bb883f55395eed30c1
Miguel Sablan <miguelsablan1@outlook.com>
2024-04-29 14:27:29 -0400

    use map function for skills

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -7,6 +7,18 @@ import { IoLogoNodejs } from "react-icons/io";
 import headshotImg from "../../assets/headshot.jpeg";
 import resumePDF from "../../assets/Miguel_Sablan_Resume.pdf";
 
+const skillsData = [
+  { title: "HTML", Icon: AiFillHtml5, color: "#e44c26" },
+  { title: "CSS", Icon: DiCss3, color: "#1573b6" },
+  { title: "JavaScript", Icon: SiJavascript, color: "#f0db4f" },
+  { title: "Git", Icon: FaGitAlt, color: "#F1502F" },
+  { title: "MongoDB", Icon: DiMongodb, color: "#4db33d" },
+  { title: "Express", Icon: SiExpress, color: "white" },
+  { title: "React", Icon: FaReact, color: "#61dafb" },
+  { title: "Node", Icon: IoLogoNodejs, color: "#83cd29" },
+  { title: "Swift", Icon: SiSwift, color: "#F1502F" },
+];
+
 const AboutSection = () => {
   return (
     <section id="about" className="about-section">
@@ -21,7 +33,7 @@ const AboutSection = () => {
             University with a Bachelor of Science in Computer Science. I am very
             passionate about creating interesting applications and enjoy
             learning the latest technologies. My hobbies include playing guitar,
-            playing video games, collecting records, and reading manga.
+            playing video games, collecting records, and learning new languages.
           </p>
 
           <p className="about-text">Check out some of my latest projects.</p>
@@ -35,44 +47,12 @@ const AboutSection = () => {
       </div>
 
       <div className="skill-summary" data-aos="fade-up" data-aos-offset="100">
-        <div className="skill">
-          <p className="skill-title">HTML</p>
-          <AiFillHtml5 size={120} color="#e44c26" className="skill-img" />
-        </div>
-        <div className="skill">
-          <p className="skill-title">CSS</p>
-          <DiCss3 size={120} color="#1573b6" className="skill-img" />
-        </div>
-        <div className="skill">
-          <p className="skill-title">JavaScript</p>
-          <SiJavascript size={120} color="#f0db4f" className="skill-img" />
-        </div>
-        <div className="skill">
-          <p className="skill-title">Git</p>
-          <FaGitAlt size={120} color="#F1502F" className="skill-img" />
-        </div>
-
-        <div className="skill">
-          <p className="skill-title">MongoDB</p>
-          <DiMongodb size={120} color="#4db33d" className="skill-img" />
-        </div>
-        <div className="skill">
-          <p className="skill-title">Express</p>
-          <SiExpress size={120} color="white" className="skill-img" />
-        </div>
-        <div className="skill">
-          <p className="skill-title">React</p>
-          <FaReact size={120} color="#61dafb" className="skill-img" />
-        </div>
-        <div className="skill">
-          <p className="skill-title">Node</p>
-          <IoLogoNodejs size={120} color="#83cd29" className="skill-img" />
-        </div>
-
-        <div className="skill">
-          <p className="skill-title">Swift</p>
-          <SiSwift size={120} color="#F1502F" className="skill-img" />
-        </div>
+        {skillsData.map((skill, index) => (
+          <div key={index} className="skill">
+            <p className="skill-title">{skill.title}</p>
+            <skill.Icon size={120} color={skill.color} className="skill-img" />
+          </div>
+        ))}
       </div>
     </section>
   );

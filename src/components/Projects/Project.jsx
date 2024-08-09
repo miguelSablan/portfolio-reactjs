197aac967011c7ee9f34e3f0ce570437894d8916
Miguel Sablan <miguelsablan1@outlook.com>
2024-08-09 17:48:05 -0400

    project link ui tweaks

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,11 +1,23 @@
-export const Project = ({ imageSrc, title, description, techStack, link, buttonText }) => (
-  <div className="project" data-aos="fade-up" data-aos-offset="100">
+import { FaArrowUpRightFromSquare } from "react-icons/fa6";
+
+export const Project = ({ imageSrc, title, description, techStack, link }) => (
+  <a
+    href={link}
+    target="_blank"
+    rel="noopener noreferrer"
+    className="project"
+    data-aos="fade-up"
+    data-aos-offset="100"
+  >
     <div className="project-image">
       <img src={imageSrc} alt={`${title} Preview Image`} />
     </div>
 
     <div className="project-info">
-      <h1>{title}</h1>
+      <div className="project-title">
+        <h1>{title}</h1>
+        <FaArrowUpRightFromSquare className="icon-link" />
+      </div>
       <p>{description}</p>
       <div className="tech-stack">
         {techStack.map((tech, index) => (
@@ -14,9 +26,6 @@ export const Project = ({ imageSrc, title, description, techStack, link, buttonT
           </span>
         ))}
       </div>
-      <a href={link} target="_blank" rel="noopener noreferrer">
-        <button className="button">{buttonText}</button>
-      </a>
     </div>
-  </div>
+  </a>
 );
--- src/components/Projects/ProjectSection.jsx
+++ src/components/Projects/ProjectSection.jsx
@@ -21,7 +21,6 @@ const ProjectSection = () => {
           description="A portfolio website to showcase my software engineering work. My first portfolio website was done with pure HTML, CSS, and JavaScript when I first started learning web development in early 2022. Fast forward a year later, I wanted to redo my portfolio using React.js."
           techStack={["React", "JavaScript", "Netlify"]}
           link=""
-          buttonText="View Site"
         />
         <Project
           imageSrc={Appli}
@@ -29,7 +28,6 @@ const ProjectSection = () => {
           description="Over a span of 9 weeks in the INIT Build program, our team of 6 people developed a full-stack web application using the MERN Stack to help Computer Science students track and manage their internship/job applications. It also provides resources in preparing for behavioral and technical interviews."
           techStack={["React", "Express", "MongoDB", "Netlify"]}
           link="https://appli-today.netlify.app/"
-          buttonText="View Site"
         />
         <Project
           imageSrc={AniMedia}
@@ -37,7 +35,6 @@ const ProjectSection = () => {
           techStack={["Swift", "Firebase", "Jikan API"]}
           description="A prototype social media application for anime fans to look up details for any anime, get the latest news on anime, post media content, and chat with other users."
           link="https://github.com/miguelSablan/AniMedia"
-          buttonText="View Repo"
         />
       </div>
     </section>

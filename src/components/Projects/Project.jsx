29d9aaa282ab43d7f2fe0b61feec1d8311f7108e
Miguel Sablan <miguelsablan1@outlook.com>
2024-08-09 16:20:47 -0400

    added project tech tags

--- src/components/Projects/Project.jsx
+++ src/components/Projects/Project.jsx
@@ -1,4 +1,4 @@
-export const Project = ({ imageSrc, title, description, link, buttonText }) => (
+export const Project = ({ imageSrc, title, description, techStack, link, buttonText }) => (
   <div className="project" data-aos="fade-up" data-aos-offset="100">
     <div className="project-image">
       <img src={imageSrc} alt={`${title} Preview Image`} />
@@ -7,6 +7,13 @@ export const Project = ({ imageSrc, title, description, link, buttonText }) => (
     <div className="project-info">
       <h1>{title}</h1>
       <p>{description}</p>
+      <div className="tech-stack">
+        {techStack.map((tech, index) => (
+          <span key={index} className="tech-item">
+            {tech}
+          </span>
+        ))}
+      </div>
       <a href={link} target="_blank" rel="noopener noreferrer">
         <button className="button">{buttonText}</button>
       </a>
--- src/components/Projects/ProjectSection.jsx
+++ src/components/Projects/ProjectSection.jsx
@@ -19,6 +19,7 @@ const ProjectSection = () => {
           imageSrc={Portfolio}
           title="Portfolio"
           description="A portfolio website to showcase my software engineering work. My first portfolio website was done with pure HTML, CSS, and JavaScript when I first started learning web development in early 2022. Fast forward a year later, I wanted to redo my portfolio using React.js."
+          techStack={["React", "JavaScript", "Netlify"]}
           link=""
           buttonText="View Site"
         />
@@ -26,12 +27,14 @@ const ProjectSection = () => {
           imageSrc={Appli}
           title="Appli"
           description="Over a span of 9 weeks in the INIT Build program, our team of 6 people developed a full-stack web application using the MERN Stack to help Computer Science students track and manage their internship/job applications. It also provides resources in preparing for behavioral and technical interviews."
+          techStack={["React", "Express", "MongoDB", "Netlify"]}
           link="https://appli-today.netlify.app/"
           buttonText="View Site"
         />
         <Project
           imageSrc={AniMedia}
           title="AniMedia"
+          techStack={["Swift", "Firebase", "Jikan API"]}
           description="A prototype social media application for anime fans to look up details for any anime, get the latest news on anime, post media content, and chat with other users."
           link="https://github.com/miguelSablan/AniMedia"
           buttonText="View Repo"

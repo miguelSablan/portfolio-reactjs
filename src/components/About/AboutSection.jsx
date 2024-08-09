b68a2014129a8ea321fcc88f9189bcfc3be124ad
Miguel Sablan <miguelsablan1@outlook.com>
2024-08-08 20:37:58 -0400

    updated skills data

--- src/components/About/AboutSection.jsx
+++ src/components/About/AboutSection.jsx
@@ -1,22 +1,38 @@
 import "./AboutSection.css";
-import { FaGitAlt, FaReact } from "react-icons/fa";
-import { AiFillHtml5, AiOutlineDownload } from "react-icons/ai";
-import { DiCss3, DiMongodb } from "react-icons/di";
-import { SiJavascript, SiExpress, SiSwift } from "react-icons/si";
+import { FaGitAlt, FaPython, FaReact } from "react-icons/fa";
+import { AiFillHtml5 } from "react-icons/ai";
+import { DiCss3, DiJava, DiMongodb } from "react-icons/di";
+import {
+  SiJavascript,
+  SiExpress,
+  SiSwift,
+  SiTypescript,
+  SiPostgresql,
+  SiNextdotjs,
+  SiTailwindcss,
+} from "react-icons/si";
 import { IoLogoNodejs } from "react-icons/io";
 import headshotImg from "../../assets/headshot.jpeg";
 import resumePDF from "../../assets/Miguel_Sablan_Resume.pdf";
 
 const skillsData = [
+  { title: "Java", Icon: DiJava, color: "orange" },
   { title: "HTML", Icon: AiFillHtml5, color: "#e44c26" },
   { title: "CSS", Icon: DiCss3, color: "#1573b6" },
   { title: "JavaScript", Icon: SiJavascript, color: "#f0db4f" },
+  { title: "TypeScript", Icon: SiTypescript, color: "#3178C6" },
+  { title: "Python", Icon: FaPython, color: "#f0db4f" },
+  { title: "Swift", Icon: SiSwift, color: "#F1502F" },
+  { title: "SQL", Icon: SiPostgresql, color: "#0064a5" },
+
+  { title: "React", Icon: FaReact, color: "#61dafb" },
+  { title: "Next", Icon: SiNextdotjs, color: "#white" },
+  { title: "Express", Icon: SiExpress, color: "white" },
+  { title: "Node", Icon: IoLogoNodejs, color: "#83cd29" },
+  { title: "TailwindCSS", Icon: SiTailwindcss, color: "#83cd29" },
+
   { title: "Git", Icon: FaGitAlt, color: "#F1502F" },
   { title: "MongoDB", Icon: DiMongodb, color: "#4db33d" },
-  { title: "Express", Icon: SiExpress, color: "white" },
-  { title: "React", Icon: FaReact, color: "#61dafb" },
-  { title: "Node", Icon: IoLogoNodejs, color: "#83cd29" },
-  { title: "Swift", Icon: SiSwift, color: "#F1502F" },
 ];
 
 const AboutSection = () => {

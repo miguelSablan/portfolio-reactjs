daf1867061987ae636ee528bd1ee862b2893db00
Miguel Sablan <miguelsablan1@outlook.com>
2024-11-08 00:16:17 -0500

    map function for projects

--- src/components/Projects/ProjectSection.jsx
+++ src/components/Projects/ProjectSection.jsx
@@ -4,6 +4,33 @@ import Appli from "../../assets/DashboardExample.png";
 import AniMedia from "../../assets/AniMedia.png";
 import { Project } from "./Project";
 
+const projects = [
+  {
+    imageSrc: Portfolio,
+    title: "Portfolio",
+    description:
+      "A portfolio website to showcase my software engineering work. My first portfolio website was done with pure HTML, CSS, and JavaScript when I first started learning web development in early 2022. Fast forward a year later, I wanted to redo my portfolio using React.js.",
+    techStack: ["React", "JavaScript", "Netlify"],
+    link: "",
+  },
+  {
+    imageSrc: Appli,
+    title: "Appli",
+    description:
+      "Over a span of 9 weeks in the INIT Build program, our team of 6 people developed a full-stack web application using the MERN Stack to help Computer Science students track and manage their internship/job applications. It also provides resources in preparing for behavioral and technical interviews.",
+    techStack: ["React", "Express", "MongoDB", "Netlify"],
+    link: "https://appli-today.netlify.app/",
+  },
+  {
+    imageSrc: AniMedia,
+    title: "AniMedia",
+    description:
+      "A prototype social media application for anime fans to look up details for any anime, get the latest news on anime, post media content, and chat with other users.",
+    techStack: ["Swift", "Firebase", "Jikan API"],
+    link: "https://github.com/miguelSablan/AniMedia",
+  },
+];
+
 const ProjectSection = () => {
   return (
     <section id="projects" className="project-section">
@@ -15,27 +42,16 @@ const ProjectSection = () => {
       </h2>
 
       <div className="projects-list">
-        <Project
-          imageSrc={Portfolio}
-          title="Portfolio"
-          description="A portfolio website to showcase my software engineering work. My first portfolio website was done with pure HTML, CSS, and JavaScript when I first started learning web development in early 2022. Fast forward a year later, I wanted to redo my portfolio using React.js."
-          techStack={["React", "JavaScript", "Netlify"]}
-          link=""
-        />
-        <Project
-          imageSrc={Appli}
-          title="Appli"
-          description="Over a span of 9 weeks in the INIT Build program, our team of 6 people developed a full-stack web application using the MERN Stack to help Computer Science students track and manage their internship/job applications. It also provides resources in preparing for behavioral and technical interviews."
-          techStack={["React", "Express", "MongoDB", "Netlify"]}
-          link="https://appli-today.netlify.app/"
-        />
-        <Project
-          imageSrc={AniMedia}
-          title="AniMedia"
-          techStack={["Swift", "Firebase", "Jikan API"]}
-          description="A prototype social media application for anime fans to look up details for any anime, get the latest news on anime, post media content, and chat with other users."
-          link="https://github.com/miguelSablan/AniMedia"
-        />
+        {projects.map((project, index) => (
+          <Project
+            key={index}
+            imageSrc={project.imageSrc}
+            title={project.title}
+            description={project.description}
+            techStack={project.techStack}
+            link={project.link}
+          />
+        ))}
       </div>
     </section>
   );
